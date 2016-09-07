90f9fb1f6aee639527e80f0e40595104dfccb868
Lane391 <robinwong51@qq.com>
2016-09-07 13:40:36 +0800

    wxjs

--- public/js/app/user/login.js
+++ public/js/app/user/login.js
@@ -19,14 +19,14 @@ define(['jquery', 'wxLogin'], function () {
             }
         });
     });
-//    var obj = new WxLogin({
-//        id: "wxLogin",
-//        appid: "wxd96928ba062cffec",
-//        scope: "snsapi_login",
-//        redirect_uri: "http%3a%2f%2firv.iresearch.com.cn%2fiResearchDataWeb%2f%3fm%3duser%26a%3dwxLoginAPI",
-//        state: "suiJi",
-//        style: "",
-//        href: ""
-//    });
-//    console.log(obj);
+   var obj = new WxLogin({
+       id: "wxLogin",
+       appid: "wxd96928ba062cffec",
+       scope: "snsapi_login",
+       redirect_uri: "http%3a%2f%2firv.iresearch.com.cn%2fiResearchDataWeb%2f%3fm%3duser%26a%3dwxLoginAPI",
+       state: "suiJi",
+       style: "",
+       href: ""
+   });
+   console.log(obj);
 })
