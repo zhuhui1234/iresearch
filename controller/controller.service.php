4ecbd731fedf7a43c39b7b4caf0668b3ec682f01
Lane391 <robinwong51@qq.com>
2016-09-09 13:22:14 +0800

    验证码

--- controller/controller.service.php
+++ controller/controller.service.php
@@ -23,6 +23,34 @@ class ServiceController extends Controller
         $res = $this->model->callBackYH();
         echo $res;
     }
-}
 
-?>
+    /**
+     *　auth code img
+     */
+    public function authImg()
+    {
+        $vcodes = '';
+        $im = imagecreate(146, 40);
+        $back = ImageColorAllocate($im, 245, 245, 245);
+        imagefill($im, 0, 0, $back); //背景
+        srand((double) microtime() * 1000000);
+        //生成4位数字
+        for ($i = 0; $i < 4; $i++) {
+            $font = ImageColorAllocate($im, rand(100, 255), rand(0, 100), rand(100, 255));
+            $authnum = rand(1, 9);
+            $vcodes .= $authnum;
+            imagestring($im, 5, 35 + $i * 20, 12, $authnum, $font);
+        }
+        for ($i = 0; $i < 100; $i++) //加入干扰象素
+        {
+            $randcolor = ImageColorallocate($im, rand(0, 255), rand(0, 255), rand(0, 255));
+            imagesetpixel($im, rand(), rand(), $randcolor);
+        }
+        @ob_clean();
+        Header("Content-type: image/PNG");
+        ImagePNG($im);
+        ImageDestroy($im);
+        Session::instance()->set('vcodes',$vcodes);
+    }
+
+}
