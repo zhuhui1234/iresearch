c1706164657732be7f1b8bace6eceea9ba61d577
Lane391 <robinwong51@qq.com>
2016-09-18 16:33:09 +0800

    login

---
+++ dev/js/app/user/login.js
@@ -0,0 +1,12 @@
+define(['helper', 'wxLogin', 'jquery'], function (Helper) {
+    //loading QRCode
+    new wxLogin({
+        id:            "wxLogin",
+        appid:         "wxd96928ba062cffec",
+        scope:         "snsapi_login",
+        redirect_uri:  "http%3a%2f%2firv.iresearch.com.cn%2fiResearchDataWeb%2f%3fm%3duser%26a%3dwxLoginAPI",
+        state:         "suiJi",
+        style:         "",
+        href:          ""
+    });
+});
