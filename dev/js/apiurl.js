ce68be8de452c8a610c85c55bc746e7a381c9b08
Lane391 <robinwong51@qq.com>
2016-09-18 10:57:15 +0800

    api uri

---
+++ dev/js/apiurl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by robinwong51 on 9/18/16.
+ */
+define({
+    wxLoginUri:'http%3a%2f%2firv.iresearch.com.cn%2fiResearchDataWeb%2f%3fm%3duser%26a%3dwxLoginAPI',
+    loginYongHongURI:'?m=user&a=loginAPI'
+});
