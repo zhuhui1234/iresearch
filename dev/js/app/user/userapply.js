3e7c881d1a37b20a5ea76d8ee5e8645a3d45eb4a
Lane391 <robinwong51@qq.com>
2016-10-27 20:54:53 +0800

    审核通过

--- dev/js/app/user/userapply.js
+++ dev/js/app/user/userapply.js
@@ -1,12 +1,16 @@
 /*! datatables */
-define(['jquery','helper','api','datatables.net','datatables.net-bs'],function($,helper,api){
-    $(function(){
+define(['jquery', 'helper', 'api', 'datatables.net', 'datatables.net-bs'], function ($, helper, api) {
+    $(function () {
         console.log('Module datatables loaded');
         $('#sidebar_applyManager').addClass('active');
-        var table = function(){
+
+        /**
+         * 权限表
+         */
+        var table = function () {
             console.log(api.getAuditList);
-            $('#user-table').DataTable( {
-                ajax:api.getAuditList,
+            $('#user-table').DataTable({
+                ajax: api.getAuditList,
                 language: {
                     "sProcessing": "处理中...",
                     "sLengthMenu": "显示 _MENU_ 项结果",
@@ -26,7 +30,7 @@ define(['jquery','helper','api','datatables.net','datatables.net-bs'],function($
                         "sNext": "<i class='fa fa-angle-right'></i>",
                         "sLast": "末页"
                     },
-                    "buttons":{
+                    "buttons": {
                         "copy": "拷贝",
                         "excel": "导出Excel",
                         "pdf": "导出PDF"
@@ -38,18 +42,47 @@ define(['jquery','helper','api','datatables.net','datatables.net-bs'],function($
                 },
                 bLengthChange: false,
                 searching: false,
-                ordering:false,
-                "columns":[
-                    {'data':'u_account'},
-                    {'data':'adt_id',mRender:function(data, type,full){
+                ordering: false,
+                drawCallback: function (settings) {
+                    $('.allow').click(function () {
+                        console.log($(this).attr('adt_id'));
+                        var ret = confirm('确定要审核通过吗？');
+                        if (ret) {
+                            console.log('ok');
+                            helper.post('upAudit', {'adt_id': $(this).attr('adt_id'), 'adt_state': 1}, function (ret) {
+                                console.log(ret);
+                            })
+                        } else {
+                            console.log('cancel');
+                        }
+                    });
+
+                    $('.deny').click(function () {
+                        var ret = confirm('确认拒绝通过该申请吗？');
+                        if (ret) {
+                            console.log('deny');
+                            helper.post('upAudit', {'adt_id': $(this).attr('adt_id'), 'adt_state': 2}, function (ret) {
+                                console.log(ret);
+                            });
+                        }
+                    });
+                },
+                "columns": [
+                    {'data': 'u_account'},
+                    {
+                        'data': 'adt_id', mRender: function (data, type, full) {
                         return '无名';
-                    }},
-                    {'data':'adt_mobile'},
-                    {'data':'cfg_name'},
-                    {'data':'adt_cdate'},
-                    {'data':'adt_id',mRender:function(data, type,full){
-                        return '<button type="button" class="btn btn-primary btn-xs mrm">通过</button><button type="button" class="btn btn-warning btn-xs">拒绝</button>';
-                    }}
+                    }
+                    },
+                    {'data': 'adt_mobile'},
+                    {'data': 'cfg_name'},
+                    {'data': 'adt_cdate'},
+                    {
+                        'data': 'adt_id', mRender: function (data, type, full) {
+                        return '<button type="button" adt_id="' + data + '" class="allow btn btn-primary btn-xs mrm">通过</button>' +
+                            '<button adt_id="' + data + '" type="button" class="btn deny btn-warning btn-xs">拒绝</button>';
+                    }
+                    }
                 ]
             });
         };
