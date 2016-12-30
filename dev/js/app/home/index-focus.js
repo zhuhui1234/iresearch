f836e29da918c5ae6d4b7b2a092e3e31665fd847
Lane391 <robinwong51@qq.com>
2016-12-30 11:30:22 +0800

    reload ajax

---
+++ dev/js/app/home/index-focus.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Hank on 2016/12/27.
+ */
+define(['app/main'], function() {
+    var rightHeight = $(".focus-main-right").height();
+    $(".focus-main-left").css("height",rightHeight);
+    $(".focus-main-right ul li a").hover(function(){
+        $(this).parent().addClass("active").siblings().removeClass("active");
+        var thisIndex = $(this).parent().index();
+        $(".focus-main-left ul li").eq(thisIndex).stop().fadeIn(200).addClass("active").siblings().stop().fadeOut(200).removeClass("active");
+    },function () {
+
+    });
+});
