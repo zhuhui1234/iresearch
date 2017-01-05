0d426971c081746b7f4c39ca708d9c1c1c680ccd
Lane391 <robinwong51@qq.com>
2017-01-05 14:34:40 +0800

    encrypt data func

--- common/common.fun.php
+++ common/common.fun.php
@@ -796,19 +796,14 @@ function jsonHead()
 function fnEncrypt($sValue, $sSecretKey)
 {
     return rtrim(
-//        base64_encode(
-        mcrypt_encrypt(
-            MCRYPT_RIJNDAEL_256,
-            $sSecretKey, $sValue,
-            MCRYPT_MODE_ECB,
-            mcrypt_create_iv(
-                mcrypt_get_iv_size(
-                    MCRYPT_RIJNDAEL_256,
-                    MCRYPT_MODE_ECB
-                ),
-                MCRYPT_RAND)
+        base64_encode(
+            mcrypt_encrypt(
+                MCRYPT_RIJNDAEL_128,
+                $sSecretKey, $sValue,
+                MCRYPT_MODE_CBC,
+                "1234567812345678"
+            )
         )
-//        )
         , "\0"
     );
 }
@@ -817,18 +812,12 @@ function fnDecrypt($sValue, $sSecretKey)
 {
     return rtrim(
         mcrypt_decrypt(
-            MCRYPT_RIJNDAEL_256,
+            MCRYPT_RIJNDAEL_128,
             $sSecretKey,
-//            base64_decode($sValue),
-            $sValue,
-            MCRYPT_MODE_ECB,
-            mcrypt_create_iv(
-                mcrypt_get_iv_size(
-                    MCRYPT_RIJNDAEL_256,
-                    MCRYPT_MODE_ECB
-                ),
-                MCRYPT_RAND
-            )
+            base64_decode($sValue),
+//            $sValue,
+            MCRYPT_MODE_CBC,
+            "1234567812345678"
         ), "\0"
     );
 }
