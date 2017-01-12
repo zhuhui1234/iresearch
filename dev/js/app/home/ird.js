34ce63ec04717bd5ddd96f159b5d00d5eae0ae19
Lane391 <robinwong51@qq.com>
2017-01-12 14:14:01 +0800

    reload ajax

---
+++ dev/js/app/home/ird.js
@@ -0,0 +1,10 @@
+/**
+ * Created by robinwong51 on 12/01/2017.
+ */
+define(['helper', 'app/main'], function (Helper) {
+    // console.log($("#mut_iFrame").attr('src'));
+    // $("#mut_iFrame").load(function(){
+    //     // console.log($(this).attr('src'));
+    //     // console.log($(this).contents());
+    // })
+});
