03607be8554cf889f97caf90e97d2a63fcde9d1d
davidwei <davidwei@iresearch.com.cn>
2017-03-22 14:58:47 +0800

    test

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ require_once("config.inc.php");
 $file_name = basename(__FILE__,'.php');
 $controller = $_request->get('m',$file_name);
 $action = $_request->get('a','index');
-//验证是否登录
+//验证是否登录-11
 Controller::instance($controller)->{$action}();
 ?>
