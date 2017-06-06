b24ecf55cc51fa8ba65ccce5509ec5a8cd3e015a
Lane391 <robinwong51@qq.com>
2017-06-06 10:12:08 +0800

    添加日志

--- dev/js/app/user/login.js
+++ dev/js/app/user/login.js
@@ -118,9 +118,9 @@ define(['helper', 'app/main', 'validator', 'canvas'], function (Helper) {
     var pdtID = Helper.getQuery('pro');
     console.log(pdtID);
     if (pdtID == null) {
-        Helper.WeChatQRCode('wxLogin', 'wxLogin', 'https://www.iresearchdata.cn/iResearchDataWeb/public/css/wechat.css');
+        Helper.WeChatQRCode('wxLogin', 'wxLogin', '//irv.iresearch.com.cn/iResearchDataWeb/public/css/wechat.css');
     } else {
-        Helper.WeChatQRCode('wxLogin', 'wxLogin', 'https://www.iresearchdata.cn/iResearchDataWeb/public/css/wechat.css', pdtID);
+        Helper.WeChatQRCode('wxLogin', 'wxLogin', '//irv.iresearch.com.cn/iResearchDataWeb/public/css/wechat.css', pdtID);
     }
 
 });
