c1643c84111f911161fff6452bb97759c9c8b371
Lane391 <robinwong51@qq.com>
2018-02-09 13:02:38 +0800

    modify links

---
+++ common/common.cache.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hi Yen Wong
+ * Date: 08/02/2018
+ * Time: 10:55 AM
+ */
+
+class CacheClass
+{
+    public $redis;
+    public function __construct()
+    {
+        $this->redis = new Redis();
+        $this->redis->connect(REDIS_SERVER, REDIS_SRV_PORT);
+        $this->redis->select(REDIS_DB);
+        return $this->redis;
+    }
+
+}
